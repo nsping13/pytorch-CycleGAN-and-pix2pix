10318d45de0ec8142f61eeabb2a16c297172d1ec
Nurit Spingarn <75696374+nsping13@users.noreply.github.com>
2023-04-22 20:40:39 +0300

    Update test.py

--- test.py
+++ test.py
@@ -82,6 +82,7 @@ if __name__ == '__main__':
                 data[key][:,:,data[key].shape[2]//2,data[key].shape[2]//2] = 1.0
 
         model.set_input(data)  # unpack data from data loader
+        print(model)
         
         model.test()           # run inference
         visuals = model.get_current_visuals()  # get image results
