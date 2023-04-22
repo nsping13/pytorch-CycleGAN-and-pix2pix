050de1e0c4cc21ec5b9d41b5b4d3f82411c31e02
Nurit Spingarn <75696374+nsping13@users.noreply.github.com>
2023-04-22 18:20:40 +0300

    Update test.py

--- test.py
+++ test.py
@@ -70,6 +70,8 @@ if __name__ == '__main__':
     for i, data in enumerate(dataset):
         if i >= opt.num_test:  # only apply our model to opt.num_test images.
             break
+        datam = torch.zeros(data.shape)
+        datam[:,:,data.shape[2]//2-4:data.shape[2]//2+4,data.shape[2]//2-4:data.shape[2]//2+4] = 1.0
         model.set_input(data)  # unpack data from data loader
         model.test()           # run inference
         visuals = model.get_current_visuals()  # get image results
